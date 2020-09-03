ce5b1748f69ec7dda4cf530f3b9fe8b7084ab35c
_Anderson_Andrade_ <31743641+AndersonAndrad@users.noreply.github.com>
2020-09-03 14:44:20 -0300

    alter gateway
    adding 'websocketserver', modify 'handle message'

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -5,6 +5,7 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WsResponse,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
@@ -12,6 +13,8 @@ import { Socket, Server } from 'socket.io';
 @WebSocketGateway()
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() wss: Server;
+
   private logger: Logger = new Logger('AppGateway');
 
   afterInit(server: Server) {
@@ -26,7 +29,9 @@ export class AppGateway
     this.logger.log(`message: ${client}`);
   }
 
-  handleMessage(client: Socket, text: string): WsResponse<string> {
-    return { event: 'messageToClient', data: text };
+  @SubscribeMessage('messaageToServer')
+  handleMessage(client: Socket, text: string): void {
+    this.wss.emit('messageToClient', text);
+    // return { event: 'messageToClient', data: text };
   }
 }
